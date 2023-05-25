21ac19447af1dca6a68bfaad802ad2419a8f7958
Claudiu-Iulian Busteaga <95749647+claudiuib@users.noreply.github.com>
2023-05-25 23:12:09 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,10 +37,10 @@ export default function Index(props:IndexItemProps) {
   </div>
 
   <div className='w-1/2 bg-[rgb(36,36,36)]'>
-    <div className='w-90'>
+    <div className='w-85'>
    
  
-    <p className='text-white text-left font-sans text-base leading-loose mt-12'>
+    <p className='text-white text-left  text-base leading-loose mt-12'>
     
     Hello! I'm passionate about web development, mobile development, machine learning, and artificial intelligence. As a web developer, I love creating dynamic and interactive websites that engage users . My interest in machine learning and AI drives me to explore innovative solutions and find ways to enhance decision-making processes. I'm constantly learning and staying up-to-date with the latest technologies in these fields to broaden my knowledge and contribute to the ever-evolving world of technology.
 </p>
