9ae9a9e6380fad2516eeeedf4e1efda6904966ff
Claudiu <busteagaclaudiu@gmail.com>
2023-05-28 20:47:21 +0100

    mobile updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,63 +6,58 @@ import Image ,{StaticImageData} from 'next/image';
 import profileImage from '../assets/image_profile.jpg'
 import Link from 'next/link';
 
-type IndexItemProps ={
-  name:string;
-  url:string;
+type IndexItemProps = {
+  name: string;
+  url: string;
+};
 
-  
-}
-
-export default function Index(props:IndexItemProps) {
-  let {url,name} = props;
+export default function Index(props: IndexItemProps) {
+  let { url, name } = props;
 
   return (
-   
-<div className='flex justify-center   bg-[rgb(36,36,36)] w-full'>
+    <div className='flex flex-col items-center bg-[rgb(36,36,36)] min-h-screen'>
+      <div className='w-full md:w-1/2'>
+        <div className='flex justify-center mt-12'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Image
+              className='rounded-full ring-1 ring-[#99999B] w-40 h-40'
+              src={profileImage}
+              alt='profile'
+              width={160}
+              height={160}
+            />
+          </motion.div>
+        </div>
+        <div className='flex justify-center'>
+          <Hero />
+        </div>
+      </div>
 
-  <div className='w-1/2 '>
-    <div className='flex justify-center mt-12'>
-      <motion.div
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Image className='rounded-full ring-1 ring-[#99999B] w-40 h-40' src={profileImage} alt='profile' />
-      </motion.div>
-    
+      <div className='w-full md:w-1/2 bg-[rgb(36,36,36)]'>
+        <div className='max-w-[400px] mx-auto px-4'>
+          <p className='text-white text-base leading-loose mt-12'>
+            Hello! I'm Claudiu, passionate about web development, mobile development, machine learning, and artificial
+            intelligence. As a web developer, I love creating dynamic and interactive websites that engage users. My
+            interest in machine learning and AI drives me to explore innovative solutions and find ways to enhance
+            decision-making processes. I'm constantly learning and staying up-to-date with the latest technologies in
+            these fields to broaden my knowledge and contribute to the ever-evolving world of technology.
+          </p>
+        </div>
+        <div className='mt-4 mx-auto'>
+          <button className='bg-[#00A67E] hover:bg-[#00614B] text-white font-bold py-2 px-4 rounded-lg'>
+            <Link href={'/contact'}>Contact Me</Link>
+          </button>
+        </div>
+
+        <div className='flex justify-center mt-20 mb-32'>
+          <SocialIcon className='mr-2 rounded-full ring-2 ring-[#99999B]' url='https://www.linkedin.com/in/claudiu-iulian-busteaga/' />
+          <SocialIcon className='mr-2 rounded-full ring-2 ring-[#99999B]' url='https://github.com/claudiuib' />
+        </div>
+      </div>
     </div>
-    <div className='flex justify-center'>
-    <Hero/>
-    </div>
-  </div>
-
-  <div className='w-1/2 bg-[rgb(36,36,36)]'>
-    <div className='w-[400px]'>
-   
- 
-    <p className='text-white text-left   text-left text-base leading-loose mt-12'>
-    
-    Hello! I'm Claudiu, passionate about web development, mobile development, machine learning, and artificial intelligence. As a web developer, I love creating dynamic and interactive websites that engage users . My interest in machine learning and AI drives me to explore innovative solutions and find ways to enhance decision-making processes. I'm constantly learning and staying up-to-date with the latest technologies in these fields to broaden my knowledge and contribute to the ever-evolving world of technology.
-</p>
-
-    
-    </div>
-    <div className='mt-4'>
-    <button className="bg-[#00A67E] hover:bg-[#00614B] text-white font-bold py-2 px-4 rounded-lg">
-      <Link href={"/contact"}>
- Contact Me
- </Link>
-</button>
-    </div>
-
-    <div className='mt-20  mb-32'>
-      <SocialIcon className=' mr-2 rounded-full ring-2 ring-[#99999B]' url="https://www.linkedin.com/in/claudiu-iulian-busteaga/" />
-      <SocialIcon className=' mr-2 rounded-full ring-2 ring-[#99999B]'url="https://github.com/claudiuib" />
-    </div>
-  </div>
-  </div> 
-
-
- 
-  )
+  );
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,44 +3,46 @@ import Image from 'next/image'
 import Crypto from '../assets/crypto_project.png'
 import Caps from '../assets/caps_compare.png'
 export default function Projects() {
-  return (
-   
-    <div className="bg-[rgb(36,36,36)] min-h-screen">
-    <div className='flex justify-center'>
-        <p className='mt-8 text-green-100 text-3xl font-semibold'>Projects</p>
-      </div>
-    <ul className="grid grid-cols-4 gap-x-12 gap-y-16 mt-8  ml-8  ">
-        <li className=' bg-green-100  rounded-sm'>
+    return (
+      <div className="bg-[rgb(36,36,36)] min-h-screen">
+        <div className="flex justify-center">
+          <p className="mt-8 text-green-100 text-3xl font-semibold">Projects</p>
+        </div>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 mx-4">
+          <li className="bg-green-100 rounded-sm">
             <a href="https://github.com/claudiuib/cw2-big-data">
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div className="px-6 py-4">
-                    <Image className='rounded-md' src={Crypto} alt='Crypto Image'/>
-                        <div className="font-bold text-xl mb-2">Crypto Compare Website</div>
-                    </div>
-                 
-                    <div className="px-6 pb-4">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 
-py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Github</span>
-                    </div>
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <Image className="rounded-md" src={Crypto} alt="Crypto Image" />
+                  <div className="font-bold text-xl mb-2">Crypto Compare Website</div>
                 </div>
+  
+                <div className="px-6 pb-4">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Github
+                  </span>
+                </div>
+              </div>
             </a>
-        </li>
-        <li className=' bg-green-100 rounded-sm'>
+          </li>
+          <li className="bg-green-100 rounded-sm">
             <a href="https://github.com/claudiuib/web-scrapper-caps">
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div className="px-6 py-4">
-                    <Image className='rounded-md' src={Caps} alt='Crypto Image'/>
-                        <div className="font-bold text-xl mb-2">Caps Web Scrapper Website</div>
-                    </div>
-                 
-                    <div className="px-6 pb-4">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 
-py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Github</span>
-                    </div>
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <Image className="rounded-md" src={Caps} alt="Crypto Image" />
+                  <div className="font-bold text-xl mb-2">Caps Web Scraper Website</div>
                 </div>
+  
+                <div className="px-6 pb-4">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Github
+                  </span>
+                </div>
+              </div>
             </a>
-        </li>
-    </ul>
-</div>
-  )
-}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  
